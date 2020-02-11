0e4571876a8a6361fc3bb3f32a3915939f9d11db
miniyass <sekri.yassine@gmail.com>
2020-02-11 14:21:12 +0100

    imbriqué navbar dans carousel

--- script.js
+++ script.js
@@ -2,6 +2,3 @@
 // window.confirm("Nous utilisons des données non sensibles comme des cookies ou des identifiants électroniques pour afficher un contenu personnalisées, mesurer le nombre de visiteurs,ect. Cliquez sur le bouton pour donner votre consentement à ces opérations et profiter d'une expérience personnalisée.");
 // $('.carousel').carousel()
 
-
-
-
