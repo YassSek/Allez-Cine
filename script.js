02c60da856bd3662af1f0a57a36e80cc34ccafc8
miniyass <sekri.yassine@gmail.com>
2020-02-11 12:18:42 +0100

    ajout d'une page test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // MESSAGE COOKIES
 // window.confirm("Nous utilisons des données non sensibles comme des cookies ou des identifiants électroniques pour afficher un contenu personnalisées, mesurer le nombre de visiteurs,ect. Cliquez sur le bouton pour donner votre consentement à ces opérations et profiter d'une expérience personnalisée.");
-$('.carousel').carousel()
+// $('.carousel').carousel()
 
 
 
