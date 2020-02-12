f85d0d813879ac7ca21d237cbc8997d340aea2ef
miniyass <sekri.yassine@gmail.com>
2020-02-12 15:17:16 +0100

    ajout des affiche , des modaux , bouton scroll et gestion api

--- form.js
+++ form.js
@@ -1,15 +1,44 @@
 // Get DOM Elements
-let corp = document.querySelector('.modal-body')
-const modal = document.querySelector('#my-modal');
-const modalBtn = document.querySelector('#modal-btn');
-const closeBtn = document.querySelector('.close');
+let corp = document.getElementById('message')
+let message = document.getElementsByClassName('modal')
+const modal = document.getElementById('my-modal');
+const modalBtn = document.getElementById('modal-btn');
+const closeBtn = document.querySelectorAll('.close');
+console.log(modal,corp,message)
+// login et register DOM
+const logBtn=document.getElementById('log-in')
 
-// Events
+const regBtn=document.getElementById('inscription')
+
+const logModal=document.getElementById('login-modal')
+
+const regModal=document.getElementById('regis-modal')
+//EVENT
+logBtn.addEventListener('click',openLogin); 
+regBtn.addEventListener('click',openRegister);
+
+// for(let btn of logBtn){
+//     btn.addEventListener('click',openLogin); // queryselectorALL selectionne tout les element mais il faut parcourir les elements d'ou la boucle FOR OF qui va reagire a chaque element BTN 
+//                                              // de l'element logBtn ( qui prends tout les selector de la class .btnConnexion) et va activer l'event listener
+// }
+
+// Events modal message
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
 
-// Open
+
+//open login & reigster
+function openRegister(){
+    regModal.style.display='block';  
+}
+function openLogin(){
+    logModal.style.display='block';
+}
+
+
+
+// Open modal message
 function openModal() {
 
     let lastName = document.getElementById('inputLastname').value;
@@ -23,14 +52,36 @@ function openModal() {
     corp.innerText = phrase;
 }
 
+
 // Close
+for(let e of closeBtn){
+    e.addEventListener('click',closeModal);
+}
+
 function closeModal() {
+
     modal.style.display = 'none';
+
+    logModal.style.display = 'none';
+
+    regModal.style.display = 'none';  
 }
 
 // Close If Outside Click
 function outsideClick(e) {
+
     if (e.target == modal) {
         modal.style.display = 'none';
     }
+    if(e.target ==logModal){
+        logModal.style.display='none';
+    }
+    if(e.target ==regModal){
+        regModal.style.display='none'; 
+    }
+
 }
+
+
+
+
--- script.js
+++ script.js
@@ -2,3 +2,123 @@
 // window.confirm("Nous utilisons des données non sensibles comme des cookies ou des identifiants électroniques pour afficher un contenu personnalisées, mesurer le nombre de visiteurs,ect. Cliquez sur le bouton pour donner votre consentement à ces opérations et profiter d'une expérience personnalisée.");
 // $('.carousel').carousel()
 
+
+
+
+// FLECHE SCROLL UP
+
+let mybutton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+ /*Scroll to top when arrow up clicked END*/
+
+
+
+
+// feature movies  
+let trends=[]
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+ 
+   
+ 
+function fetchFilms(){//RECUPERE 2O FILMS avec leurs infos de base
+   
+    const fims= fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=5c717347fca7ad27bbc4791dbc618e51", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        Array.from(result.results).forEach( el=>{
+            trends.push([el.id,el.title,el.release_date,el.poster_path,el.genre_ids])
+            })
+        AddGenre(trends)
+    })
+   
+ 
+}
+ 
+function AddGenre(arr){//Change les id de genre directement par les noms de genres
+    genrebyid=[]
+    let genre= fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=5c717347fca7ad27bbc4791dbc618e51&language=en-US", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        Array.from(result.genres).forEach( el=>{
+            genrebyid.push(el)
+            })
+        for(el in arr){
+            for(e in (arr[el][4])){
+                genre=genrebyid.find(x => x.id == arr[el][4][e])
+                arr[el][4][e]=genre.name
+            }
+        }
+        createCards(arr)
+        console.log(arr)
+    })
+}
+ 
+function createCards(arr){
+    let target = document.getElementById("target")
+    let template = document.getElementById("tpl")
+    for (ele of arr){
+        let clone = document.importNode(template.content, true)
+        console.log(clone)
+        //ID DU FILM
+        let idfilm=clone.getElementById("card")
+        idfilm.setAttribute("value",ele[0])
+        //Titre du film
+        let title = clone.querySelectorAll("h5")
+        title[0].textContent = ele[1]
+        //Poster du film
+        let image=clone.querySelectorAll("img")
+        image[0].setAttribute("src",'https://image.tmdb.org/t/p/w500'+ele[3])
+        //Genres
+        let genre = clone.querySelectorAll("p")
+        genre[0].textContent = ele[4].join(', ')
+        let date = clone.querySelectorAll("i")
+        date[0].textContent = ele[2]
+        target.appendChild(clone)
+    }
+}
+ 
+ 
+fetchFilms()
+document.getElementById("action").addEventListener('click',()=>{
+    console.log('ACTION')
+    let cards=document.getElementsByClassName('cds')
+    for(i=0;i<cards.length;i++){
+        par=card[i].querySelectorAll("p")
+        if(!par[0].textContent.includes('Action')){
+            cards[i].style.display="none"
+        }else{
+            cards[i].style.display="initial"
+        }
+    }
+})
+document.getElementById("drama").addEventListener('click',()=>{
+    console.log('DRAMA')
+    let cards=document.getElementsByClassName('cds')
+    for(i=0;i<cards.length;i++){
+        par=card[i].querySelectorAll("p")
+        if(!par[0].textContent.includes('Drama')){
+            cards[i].style.display="none"
+        }else{
+            cards[i].style.display="initial"
+        }
+    }
+})
