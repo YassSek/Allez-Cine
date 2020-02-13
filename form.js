d6816b0719aa46add0fe64320da53df34a79ad70
miniyass <sekri.yassine@gmail.com>
2020-02-13 11:31:23 +0100

    ajout des derniere modif

--- form.js
+++ form.js
@@ -3,26 +3,28 @@
 let mybutton = document.getElementById("scrollBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
- /*Scroll to top when arrow up clicked END*/
+/*Scroll to top when arrow up clicked END*/
 
-const closeBtn = document.getElementsByClassName('close');
 
-// Get DOM Elements
+
+// message modal DOM Elements
 let corp = document.getElementById('message')
 let message = document.getElementsByClassName('modal')
 
@@ -30,33 +32,36 @@ const modal = document.getElementById('my-modal');
 const modalBtn = document.getElementById('modal-btn');
 
 // login et register DOM
-const logBtn=document.getElementById('log-in')
+const logBtn = document.getElementById('log-in')
 
-const regBtn=document.getElementById('inscription')
+const regBtn = document.getElementById('inscription')
 
-const logModal=document.getElementById('login-modal')
+const logModal = document.getElementById('login-modal')
 
-const regModal=document.getElementById('regis-modal')
+const regModal = document.getElementById('regis-modal')
 //EVENT
-logBtn.addEventListener('click',openLogin); 
-regBtn.addEventListener('click',openRegister);
+logBtn.addEventListener('click', openLogin);
+regBtn.addEventListener('click', openRegister);
+modalBtn.addEventListener('click', openModal);
+
+window.addEventListener('click', outsideClick);
 
 // for(let btn of logBtn){
 //     btn.addEventListener('click',openLogin); // queryselectorALL selectionne tout les element mais il faut parcourir les elements d'ou la boucle FOR OF qui va reagire a chaque element BTN 
 //                                              // de l'element logBtn ( qui prends tout les selector de la class .btnConnexion) et va activer l'event listener
 // }
 // Events modal message
-modalBtn.addEventListener('click', openModal);
 
-window.addEventListener('click', outsideClick);
+
 
 
 //open login & reigster
-function openRegister(){
-    regModal.style.display='block';  
+function openRegister() {
+    regModal.style.display = 'block';
 }
-function openLogin(){
-    logModal.style.display='block';
+
+function openLogin() {
+    logModal.style.display = 'block';
 }
 
 // Open modal message
@@ -75,8 +80,12 @@ function openModal() {
 
 
 // Close
-for(let e of closeBtn){
-    e.addEventListener('click',closeModal);
+
+const closeBtn = document.getElementsByClassName('close');
+
+
+for (let e of closeBtn) {
+    e.addEventListener('click', closeModal);
 }
 
 function closeModal() {
@@ -85,7 +94,7 @@ function closeModal() {
 
     logModal.style.display = 'none';
 
-    regModal.style.display = 'none';  
+    regModal.style.display = 'none';
 }
 
 // Close If Outside Click
@@ -94,11 +103,11 @@ function outsideClick(e) {
     if (e.target == modal) {
         modal.style.display = 'none';
     }
-    if(e.target ==logModal){
-        logModal.style.display='none';
+    if (e.target == logModal) {
+        logModal.style.display = 'none';
     }
-    if(e.target ==regModal){
-        regModal.style.display='none'; 
+    if (e.target == regModal) {
+        regModal.style.display = 'none';
     }
 
 }
--- script.js
+++ script.js
@@ -2,29 +2,30 @@
 // window.confirm("Nous utilisons des données non sensibles comme des cookies ou des identifiants électroniques pour afficher un contenu personnalisées, mesurer le nombre de visiteurs,ect. Cliquez sur le bouton pour donner votre consentement à ces opérations et profiter d'une expérience personnalisée.");
 // $('.carousel').carousel()
 
-
-// feature movies  
-let trends=[] // array qui va prendre chaque element et leur differente valeurs ( genre , poster , date ) a chaqu'un des ellement disponible
+let trends=[]
+let shows=6
 const requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
- 
+
+    
+
 function fetchFilms(){//RECUPERE 2O FILMS avec leurs infos de base
-   
+    
     const fims= fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=5c717347fca7ad27bbc4791dbc618e51", requestOptions)
     .then(response => response.json())
     .then(result => {
         Array.from(result.results).forEach( el=>{
             trends.push([el.id,el.title,el.release_date,el.poster_path,el.genre_ids])
             })
-        AddGenre(trends) // appel de la fct addGenre avec le tableau et toute les valeur introduite dedans
+        AddGenre(trends)
     })
-   
- 
+    
+  
 }
- 
-function AddGenre(arr){//Change les id de genre directement par les noms de genres
+
+function AddGenre(arr){//Change les id de genre directement par les noms de genres 
     genrebyid=[]
     let genre= fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=5c717347fca7ad27bbc4791dbc618e51&language=en-US", requestOptions)
     .then(response => response.json())
@@ -34,45 +35,65 @@ function AddGenre(arr){//Change les id de genre directement par les noms de genr
             })
         for(el in arr){
             for(e in (arr[el][4])){
-                genre=genrebyid.find(x => x.id == arr[el][4][e]) // sa faut m'expliquer forttttt !!!!!!!!!!!
+                genre=genrebyid.find(x => x.id == arr[el][4][e])
                 arr[el][4][e]=genre.name
             }
         }
         createCards(arr)
-        // console.log(arr)
+        console.log(arr)
     })
 }
- 
+
 function createCards(arr){
     let target = document.getElementById("target")
-    let template = document.getElementById("tpl") // telecharge la template = de la structure html
+    let template = document.getElementById("tpl")
+    let i=0
     for (ele of arr){
-        let clone = document.importNode(template.content, true) // variable clone qui recrée la template 
-        // console.log(clone)
+        let clone = document.importNode(template.content, true)
+        if(i>=6){
+            let disp=clone.querySelectorAll('div')
+            disp[0].style.display='none'
+        }
+        i++
         //ID DU FILM
         let idfilm=clone.getElementById("card")
-        idfilm.setAttribute("value",ele[0])//ajoute l'element id du film a l'element card
+        idfilm.setAttribute("value",ele[0])
         //Titre du film
         let title = clone.querySelectorAll("h5")
-        title[0].textContent = ele[1] 
+        title[0].textContent = ele[1]
         //Poster du film
         let image=clone.querySelectorAll("img")
-        image[0].setAttribute("src",'https://image.tmdb.org/t/p/w500'+ele[3]) // chaque element du tableau arr dispose a son tour d'un tableau avec des position fixe ici la pos 3 est pour selectionner l'image
+        image[0].setAttribute("src",'https://image.tmdb.org/t/p/w500'+ele[3])
         //Genres
         let genre = clone.querySelectorAll("p")
-        genre[0].textContent = ele[4].join(', ') // ajoute l'element n 4 et le transforme en string dans le selecteur genre
+        genre[0].textContent = ele[4].join(', ')
         let date = clone.querySelectorAll("i")
-        date[0].textContent = ele[2] // ici ajoute l'element de la date en pos 2 pour lui indiqué la date de l'element i ( mais pq balise i ??)
+        date[0].textContent = ele[2]
         target.appendChild(clone)
     }
 }
- 
- 
+
 fetchFilms()
+
+
+
+async function fetchTrailer(id){
+    let res= await fetch("http://api.themoviedb.org/3/movie/"+id+"/videos?api_key=5c717347fca7ad27bbc4791dbc618e51", requestOptions)
+    .then(response => response.json())
+    .then(result => result)
+    .catch(error => console.log('error', error));
+    document.getElementById('iframeboy').setAttribute('src',"https://www.youtube.com/embed/"+res.results[0].key)
+    setTimeout(function(){ document.getElementById('modalvid').style.display='block'}, 500)
+
+}
+
+
 document.getElementById("action").addEventListener('click',()=>{
-    // console.log('ACTION')
+    console.log('ACTION')
+    document.getElementById('showmore').style.display='none'
+    document.getElementById('showless').style.display='none'
     let cards=document.getElementsByClassName('cds')
-    for(i=0;i<cards.length;i++){ // parcours les cartes de chaque film afficher
+    for(i=0;i<cards.length;i++){
         par=card[i].querySelectorAll("p")
         if(!par[0].textContent.includes('Action')){
             cards[i].style.display="none"
@@ -82,7 +103,9 @@ document.getElementById("action").addEventListener('click',()=>{
     }
 })
 document.getElementById("drama").addEventListener('click',()=>{
-    // console.log('DRAMA')
+    console.log('DRAMA')
+    document.getElementById('showmore').style.display='none'
+    document.getElementById('showless').style.display='none'
     let cards=document.getElementsByClassName('cds')
     for(i=0;i<cards.length;i++){
         par=card[i].querySelectorAll("p")
@@ -96,10 +119,79 @@ document.getElementById("drama").addEventListener('click',()=>{
 
 document.getElementById("all").addEventListener('click',()=>{
     // console.log('DRAMA')
+    document.getElementById('showmore').style.display='initial'
+    document.getElementById('showless').style.display='none'
     let cards=document.getElementsByClassName('cds')
+    shows=6
     for(i=0;i<cards.length;i++){
-        par=card[i].querySelectorAll("p")
-        cards[i].style.display="initial";
+        if(i<6){
+            cards[i].style.display="initial";
+        }else{
+            cards[i].style.display="none";
+        }
         
     }
 })
+
+document.getElementById("showmore").addEventListener('click',()=>{
+    document.getElementById('showless').style.display='inline'
+    let cards=document.getElementsByClassName('cds')
+        for(i=shows;i<shows+6;i++){
+            if(i<cards.length)
+                cards[i].style.display="initial"
+        }
+        if(shows+6<cards.length){
+            shows+=6
+        }else if(shows<cards.length){
+            shows+=cards.length-shows
+        } 
+    }
+    )
+
+document.getElementById("showless").addEventListener('click',()=>{
+    let cards=document.getElementsByClassName('cds')
+        if(shows>6){
+            if(shows%6==0){
+                for(j=shows;j>=shows-6;j--){
+                    cards[j].style.display="none"
+                }
+                shows-=6
+            }else{
+                mod=shows%6
+                for(x=shows-1;x>=shows-mod;x--){
+                    cards[x].style.display="none"
+                }
+                shows-=mod
+            }
+            
+        }
+        if(shows<=6){
+            document.getElementById('showless').style.display='none'
+        }
+})
+
+
+
+
+setTimeout(function(){ Array.from(document.querySelectorAll('.card')).forEach(function(item) {
+    item.addEventListener('click', function() {
+      fetchTrailer(item.getAttribute('value'))
+    });
+     });; }, 1000)
+
+
+
+function closeModal() {
+    document.getElementById('modalvid').style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+    if (e.target == modalvid) {
+        document.getElementById('modalvid').style.display = 'none';
+    }   
+}
+
+document.getElementById('closevideo').addEventListener('click',closeModal);
+window.addEventListener('click', outsideClick);
+
